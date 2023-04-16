1ac9aa2e238f36f9249699cc4382449139a1fa2a
KevinMartinezMella <iamkamm7@gmail.com>
2023-04-15 20:16:49 -0400

    Página Base

---
+++ archivos/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.carruselPrincipal').slick({
+        dots: false,
+        prevArrow: false,
+        nextArrow: false,
+        slidesToShow: 1,
+        centerMode: false,
+        autoplay: true,
+        autoplaySpeed: 1000,
+    });
+});
