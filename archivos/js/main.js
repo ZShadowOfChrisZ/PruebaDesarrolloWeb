7fc09a998e73a62bc4134d053977afaa5e8ed19e
KevinMartinezMella <iamkamm7@gmail.com>
2023-04-16 03:07:40 -0400

    Comienzo de Tienda

--- archivos/js/main.js
+++ archivos/js/main.js
@@ -1,11 +1,49 @@
 $(document).ready(function(){
+    // $('.carruselPrincipal').slick({
+    //     dots: false,
+    //     prevArrow: false,
+    //     nextArrow: false,
+    //     slidesToShow: 1,
+    //     centerMode: false,
+    //     autoplay: true,
+    //     autoplaySpeed: 1000,
+    // });
+
     $('.carruselPrincipal').slick({
         dots: false,
-        prevArrow: false,
-        nextArrow: false,
-        slidesToShow: 1,
-        centerMode: false,
         autoplay: true,
         autoplaySpeed: 1000,
-    });
+        // speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: false,
+        responsive: [
+            {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+            },
+            {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    })
 });
