e7e19993a0734cbc77f4858fbd43b976ea8e35b5
Brandon Martinez <apink.hayoung.net@gmail.com>
2023-04-16 23:30:16 -0400

    Contacto y validaciones

--- archivos/js/main.js
+++ archivos/js/main.js
@@ -46,4 +46,80 @@ $(document).ready(function(){
             // instead of a settings object
         ]
     })
+    $("#boton").click(function(){
+        if($("#nombre").val().length<3){
+            $("#nombre").css("border","2px solid red");
+            toastMixin.fire({
+                title: 'Los nombres debe tener más de 3 carácteres',
+                icon: 'error'
+              });
+        
+        } else if($("#apellido").val().length<3){
+            toastMixin.fire({
+                title: 'Los apellidos debe tener más de 3 carácteres',
+                icon: 'error'
+              });
+            $("#nombre").css("border","2px solid #fff");
+            $("#apellido").css("border","2px solid red");
+        
+        }else if($("#email").val().length<3){
+            toastMixin.fire({
+                title: 'El email debe tener más de 3 carácteres',
+                icon: 'error'
+              });
+            $("#nombre").css("border","2px solid #fff");
+            $("#apellido").css("border","2px solid #fff");
+            $("#email").css("border","2px solid red");
+        
+        }else if($("#telefono").val().length<9){
+            toastMixin.fire({
+                title: 'El teléfono debe tener 8 dígitos',
+                icon: 'error'
+              });
+            $("#nombre").css("border","2px solid #fff");
+            $("#apellido").css("border","2px solid #fff");
+            $("#email").css("border","2px solid #fff");
+            $("#telefono").css("border","2px solid red");
+        
+        }else if($("#ciudad").val()==""){
+
+            $("#nombre").css("border","2px solid #fff");
+            $("#apellido").css("border","2px solid #fff");
+            $("#email").css("border","2px solid #fff");
+            $("#telefono").css("border","2px solid #fff");
+            $("#ciudad").css("border","1px solid red");
+
+        }else{
+            $("#nombre").css("border","1px solid green");
+            $("#apellido").css("border","1px solid green");
+            $("#email").css("border","1px solid green");
+            $("#telefono").css("border","1px solid green");
+            $("#ciudad").css("border","1px solid green");
+            
+            $("#nombre").val("");
+            $("#apellido").val("");
+            $("#email").val("");
+            $("#telefono").val("");
+            $("#ciudad").val("");
+            
+            toastMixin.fire({
+                animation: true,
+                title: 'Formulario enviado'
+              }); 
+        }
+    });
 });
+var toastMixin = Swal.mixin({
+    toast: true,
+    icon: 'success',
+    title: 'General Title',
+    animation: false,
+    position: 'top-right',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  });
